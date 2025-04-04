69f1ce5060564a9d0a2b550689e631f451a43cd9
Rachel Lazarus <racheylazarus@gmail.com>
2025-04-04 14:59:48 +0200

    Add composer files and vendor directory

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f72a2524879f87c69f4c271f6c4d0623f0f9d7c0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f72a2524879f87c69f4c271f6c4d0623f0f9d7c0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
